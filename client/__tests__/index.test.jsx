c7c7b060d3eff380233c2d8c9f565a36a96bc1d2
dhgu-dev <82129110+dhgu-dev@users.noreply.github.com>
2022-09-24 14:43:06 +0900

    client project default settings (#1)
    settings
    - yarn, create-next-app, typescript, eslint, prettier, test, docker, 빌드 자동화

---
+++ client/__tests__/index.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Index from '../pages/index';
+
+describe('Home', () => {
+  it('renders a heading', () => {
+    render(<Index />);
+
+    const heading = screen.getByRole('heading', {
+      name: /welcome to next\.js!/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
