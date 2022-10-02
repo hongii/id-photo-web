91fbb92f1051aa42663753c3f58cc59304f1873a
hongii <93701887+hongii@users.noreply.github.com>
2022-10-02 21:35:57 +0900

    Home component css styling (#6)
    * add: images folder add
    
    * Style: globals.css change
    
    * Add: className add in index.tsx(Home component)
    
    * Style: Home component css styling(css-module)

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -4,7 +4,7 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => (
-  <div className={styles.container}>
+  <div>
     <Head>
       <title>ID Photo Web</title>
       <meta name="description" content="id photo generatation service" />
@@ -14,19 +14,46 @@ const Home: NextPage = () => (
       />
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    <h1>서비스 소개</h1>
-    <p>헤어 스타일을 바꾸고 싶으신가요? 지금 바로 시작해 보세요!</p>
-    <label htmlFor="photoUpload">
-      시작하기
-      <input
-        type="file"
-        name="photo"
-        id="photoUpload"
-        accept="image/*"
-        aria-label="사진 업로드"
-        hidden
-      />
-    </label>
+
+    <h1 className={styles['screen-reader-only']}>서비스 소개</h1>
+
+    <div className={styles.container}>
+      <p className={styles.intro}>
+        헤어 스타일을 바꾸고 싶으신가요? 지금 바로 시작해 보세요!
+      </p>
+
+      <div className={styles['picture-container']}>
+        <img
+          className={styles.picture1}
+          src="/images/picture1.png"
+          alt="picture1"
+        />
+        <img
+          className={styles.picture2}
+          src="/images/picture2.png"
+          alt="picture2"
+        />
+        <img
+          className={styles.picture3}
+          src="/images/picture3.png"
+          alt="picture3"
+        />
+      </div>
+
+      <div className={styles['start-btn']}>
+        <label className={styles['start-btn__label']} htmlFor="photoUpload">
+          시작하기
+          <input
+            type="file"
+            name="photo"
+            id="photoUpload"
+            accept="image/*"
+            aria-label="사진 업로드"
+            hidden
+          />
+        </label>
+      </div>
+    </div>
   </div>
 );
 
