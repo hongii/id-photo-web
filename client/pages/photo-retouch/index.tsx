e6d7b0b5a680f874fb7904323a3a98326bce1f58
khe <khj778878@naver.com>
2022-10-21 05:13:56 +0900

    Style:PhotoRetouch css styling and RetouchType component creation

--- client/pages/photo-retouch/index.tsx
+++ client/pages/photo-retouch/index.tsx
@@ -1,15 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import Header from '@/components/Header';
+import RetouchTypes from '@/components/RetouchTypes';
 import { useRouter } from 'next/router';
 import { useRecoilValue } from 'recoil';
 import { withSrc } from 'recoil/faceImage';
+import styles from './PhotoRetouch.module.css';
 
 const PhotoRetouch: NextPage = () => {
   const faceSrc = useRecoilValue(withSrc);
   const router = useRouter();
+  const [activeType, setActiveType] = useState(0);
+  const types: string[] = ['미백', '갸름하게'];
+
+  const onClickHandler = (idx: number) => {
+    if (activeType !== idx) {
+      setActiveType(idx);
+    }
+  };
+
+  useEffect(() => {
+    setActiveType(0);
+  }, []);
 
   useEffect(() => {
     if (faceSrc === '/') {
@@ -18,7 +32,7 @@ const PhotoRetouch: NextPage = () => {
   }, [faceSrc, router]);
 
   return (
-    <div>
+    <div className={styles['page-layout']}>
       <Head>
         <title>ID Photo Web</title>
         <meta name="description" content="id photo generatation service" />
@@ -33,29 +47,36 @@ const PhotoRetouch: NextPage = () => {
         href="/background-decision"
         onClickButton={() => router.push('/photo-save')}
       />
-      <main>
-        <Image
-          src="/vercel.svg"
-          alt="얼굴 사진 결과물"
-          width="43"
-          height="42"
-        />
-        <section>
-          <h2>보정 종류 선택</h2>
-          <ul aria-label="종류 목록">
-            <li aria-label="type item">
-              <button type="button">미백</button>
-              <i>|</i>
-            </li>
-          </ul>
-        </section>
-        <section>
-          <h2>보정하기</h2>
-          {/* eslint jsx-a11y/label-has-associated-control: ["error", { assert: "either" } ] */}
-          <label htmlFor="ratio">0</label>
-          <input type="range" name="ratio" id="ratio" min="0" max="100" />
-          <label htmlFor="ratio">100</label>
-        </section>
+      <main className={styles.main}>
+        <div className={styles['face-image-container']}>
+          <div className={styles['face-image']}>
+            <Image src={faceSrc} alt="얼굴 사진 결과물" layout="fill" />
+          </div>
+        </div>
+        <article className={styles['select-container']}>
+          <section>
+            <h2 className={styles['screen-reader-only']}>보정 종류 선택</h2>
+            <ul aria-label="종류 목록" className={styles['type-select']}>
+              {types.map((typeName, idx) => (
+                <RetouchTypes
+                  key={`KEY_${typeName}`}
+                  typeName={typeName}
+                  idx={idx}
+                  types={types}
+                  activeType={activeType}
+                  onClickHandler={onClickHandler}
+                />
+              ))}
+            </ul>
+          </section>
+          <section>
+            <h2 className={styles['screen-reader-only']}>보정하기</h2>
+            {/* eslint jsx-a11y/label-has-associated-control: ["error", { assert: "either" } ] */}
+            <label htmlFor="ratio">0</label>
+            <input type="range" name="ratio" id="ratio" min="0" max="100" />
+            <label htmlFor="ratio">100</label>
+          </section>
+        </article>
       </main>
     </div>
   );
