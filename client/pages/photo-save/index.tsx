1f29c71d33a478d4a62a585e0bb19bd06ed25990
dhgu-dev <82129110+dhgu-dev@users.noreply.github.com>
2022-10-26 16:55:23 +0900

    8주차 작업 (#13)
    * feat: Make canvas to blob
    
    * feat: Add file download
    
    * feat: Adjust download img ratio

--- client/pages/photo-save/index.tsx
+++ client/pages/photo-save/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
@@ -6,10 +6,12 @@ import Header from '@/components/Header';
 import { useRouter } from 'next/router';
 import { useRecoilValue } from 'recoil';
 import { withSrc } from 'recoil/faceImage';
+import styles from '@/styles/PhotoSave.module.css';
 
 const PhotoSave: NextPage = () => {
   const faceSrc = useRecoilValue(withSrc);
   const router = useRouter();
+  const [downloadURL, setDownloadURL] = useState(faceSrc);
 
   useEffect(() => {
     if (faceSrc === '/') {
@@ -17,8 +19,23 @@ const PhotoSave: NextPage = () => {
     }
   }, [faceSrc, router]);
 
+  useEffect(() => {
+    const img = document.createElement('img');
+    const [w, h] = [3.5, 4.5];
+    img.onload = () => {
+      const canvas = document.createElement('canvas');
+      canvas.width = img.naturalWidth;
+      canvas.height = img.naturalWidth * (h / w);
+      const ctx = canvas.getContext('2d');
+      ctx?.drawImage(img, 0, 0, img.naturalWidth, img.naturalWidth * (h / w));
+      const url = canvas.toDataURL();
+      setDownloadURL(url);
+    };
+    img.src = faceSrc;
+  }, [faceSrc]);
+
   return (
-    <div>
+    <div className={styles['page-layout']}>
       <Head>
         <title>ID Photo Web</title>
         <meta name="description" content="id photo generatation service" />
@@ -29,14 +46,21 @@ const PhotoSave: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header title="사진 저장" href="/photo-retouch" toMain />
-      <main>
-        <Image
-          src="/vercel.svg"
-          alt="얼굴 사진 결과물"
-          width="43"
-          height="42"
-        />
-        <button type="button">저장하기</button>
+      <main className={styles.main}>
+        <div className={styles['face-image-container']}>
+          <div className={styles['face-image']}>
+            <Image src={faceSrc} alt="얼굴 사진 결과물" layout="fill" />
+          </div>
+        </div>
+        <div className={styles['download-container']}>
+          <a
+            href={downloadURL}
+            download={`id-photo-result-${Date.now()}`}
+            className={styles.download}
+          >
+            저장하기
+          </a>
+        </div>
       </main>
     </div>
   );
