629afe52f6cf7caa469766091d6990b5449e4b94
khe <khj778878@naver.com>
2022-11-11 00:09:56 +0900

    Fix: lint error fixed

--- client/pages/photo-retouch/index.tsx
+++ client/pages/photo-retouch/index.tsx
@@ -7,22 +7,24 @@ import RetouchTypes from '@/components/RetouchTypes';
 import { useRouter } from 'next/router';
 import { useRecoilValue } from 'recoil';
 import { withSrc } from 'recoil/faceImage';
+// import axios from 'axios';
 import styles from './PhotoRetouch.module.css';
-import axios from 'axios';
+
 
 const PhotoRetouch: NextPage = () => {
   const faceSrc = useRecoilValue(withSrc);
   const router = useRouter();
   const [activeType, setActiveType] = useState(0);
   const types: string[] = ['미백', '갸름하게'];
-  //const BEAUTY_API_KEY = process.env.BEAUTY_API_KEY;
+  
+  // const BEAUTY_API_KEY = process.env.BEAUTY_API_KEY;
 
-  //const handleOnChange = () => {
-    //let fileInput = document.getElementById("fileInput");
-    //값이 변경될때 호출 되는 이벤트 리스너
-    //console.log(fileInput.files)
-    
-    /* 스킨 뷰티 기능 */
+  // const handleOnChange = () => {
+  // let fileInput = document.getElementById("fileInput");
+  // 값이 변경될때 호출 되는 이벤트 리스너
+  // console.log(fileInput.files)
+
+  /* 스킨 뷰티 기능 */
   //   const data = new FormData();
   //   data.append("image_target", fileInput.files[0], "sample.png");
 
@@ -41,29 +43,29 @@ const PhotoRetouch: NextPage = () => {
   //   }).catch(function (error) {
   //     console.error(error);
   //   });
-//   // }
+  //   // }
 
-    /* 슬리밍 기능 */
-    // const data = new FormData();
-    // data.append("image", fileInput.files[0], "sample.png");
-    // data.append("slim_degree", "2.0");
+  /* 슬리밍 기능 */
+  // const data = new FormData();
+  // data.append("image", fileInput.files[0], "sample.png");
+  // data.append("slim_degree", "2.0");
 
-    // const options = {
-    //   method: 'POST',
-    //   url: 'https://ai-face-slimming.p.rapidapi.com/face/editing/liquify-face',
-    //   headers: {
-    //     'X-RapidAPI-Key': 'BEAUTY_API_KEY',
-    //     'X-RapidAPI-Host': 'ai-face-slimming.p.rapidapi.com'
-    //   },
-    //   data: data
-    // };
+  // const options = {
+  //   method: 'POST',
+  //   url: 'https://ai-face-slimming.p.rapidapi.com/face/editing/liquify-face',
+  //   headers: {
+  //     'X-RapidAPI-Key': 'BEAUTY_API_KEY',
+  //     'X-RapidAPI-Host': 'ai-face-slimming.p.rapidapi.com'
+  //   },
+  //   data: data
+  // };
 
-    // axios.request(options).then(function (response) {
-    //   console.log(response.data);
-    // }).catch(function (error) {
-    //   console.error(error);
-    // });
-  //}
+  // axios.request(options).then(function (response) {
+  //   console.log(response.data);
+  // }).catch(function (error) {
+  //   console.error(error);
+  // });
+  // }
 
   const onClickHandler = (idx: number) => {
     if (activeType !== idx) {
@@ -126,7 +128,14 @@ const PhotoRetouch: NextPage = () => {
             {/* eslint jsx-a11y/label-has-associated-control: ["error", { assert: "either" } ] */}
             {/* <input type="file" id='fileInput' onChange={ handleOnChange}></input> */}
             <label htmlFor="ratio">0</label>
-            <input className={styles.ratio }type="range" name="ratio" id="ratio" min="0" max="100"/>
+            <input
+              className={styles.ratio}
+              type="range"
+              name="ratio"
+              id="ratio"
+              min="0"
+              max="100"
+            />
             <label htmlFor="ratio">100</label>
           </section>
         </article>
