157c72c9fb256c34149c90915a639cb48f9a2c61
hongii <93701887+hongii@users.noreply.github.com>
2022-11-11 16:24:40 +0900

    page5 - PhotoRetouch (#15)
    * Style:PhotoRetouch css styling and RetouchType component creation
    
    * Fix: PhotoRetouch test code is modified-test passed
    
    * Feat: page4-TypeList component code is modified(import classnames)
    
    * Feat: page2-HairStyleList component code is modified(import classnames) and remove utils/classList.ts
    
    * Style: ratio css modified (and temporary api code)
    
    * Fix: lint error fixed

--- client/pages/photo-retouch/index.tsx
+++ client/pages/photo-retouch/index.tsx
@@ -1,15 +1,81 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import Header from '@/components/Header';
+import RetouchTypes from '@/components/RetouchTypes';
 import { useRouter } from 'next/router';
 import { useRecoilValue } from 'recoil';
 import { withSrc } from 'recoil/faceImage';
+// import axios from 'axios';
+import styles from './PhotoRetouch.module.css';
+
 
 const PhotoRetouch: NextPage = () => {
   const faceSrc = useRecoilValue(withSrc);
   const router = useRouter();
+  const [activeType, setActiveType] = useState(0);
+  const types: string[] = ['미백', '갸름하게'];
+  
+  // const BEAUTY_API_KEY = process.env.BEAUTY_API_KEY;
+
+  // const handleOnChange = () => {
+  // let fileInput = document.getElementById("fileInput");
+  // 값이 변경될때 호출 되는 이벤트 리스너
+  // console.log(fileInput.files)
+
+  /* 스킨 뷰티 기능 */
+  //   const data = new FormData();
+  //   data.append("image_target", fileInput.files[0], "sample.png");
+
+  //   const options = {
+  //     method: 'POST',
+  //     url: 'https://photo-retouching.p.rapidapi.com/huoshan/facebody/facepretty',
+  //     headers: {
+  //       'X-RapidAPI-Key': BEAUTY_API_KEY,
+  //       'X-RapidAPI-Host': 'photo-retouching.p.rapidapi.com'
+  //     },
+  //     data: data
+  //   };
+
+  //   axios.request(options).then(function (response) {
+  //     console.log(response.data);
+  //   }).catch(function (error) {
+  //     console.error(error);
+  //   });
+  //   // }
+
+  /* 슬리밍 기능 */
+  // const data = new FormData();
+  // data.append("image", fileInput.files[0], "sample.png");
+  // data.append("slim_degree", "2.0");
+
+  // const options = {
+  //   method: 'POST',
+  //   url: 'https://ai-face-slimming.p.rapidapi.com/face/editing/liquify-face',
+  //   headers: {
+  //     'X-RapidAPI-Key': 'BEAUTY_API_KEY',
+  //     'X-RapidAPI-Host': 'ai-face-slimming.p.rapidapi.com'
+  //   },
+  //   data: data
+  // };
+
+  // axios.request(options).then(function (response) {
+  //   console.log(response.data);
+  // }).catch(function (error) {
+  //   console.error(error);
+  // });
+  // }
+
+  const onClickHandler = (idx: number) => {
+    if (activeType !== idx) {
+      setActiveType(idx);
+    }
+  };
+
+  useEffect(() => {
+    setActiveType(0);
+  }, []);
 
   useEffect(() => {
     if (faceSrc === '/') {
@@ -18,7 +84,7 @@ const PhotoRetouch: NextPage = () => {
   }, [faceSrc, router]);
 
   return (
-    <div>
+    <div className={styles['page-layout']}>
       <Head>
         <title>ID Photo Web</title>
         <meta name="description" content="id photo generatation service" />
@@ -28,34 +94,51 @@ const PhotoRetouch: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <Header
         title="사진 보정"
         href="/background-decision"
         onClickButton={() => router.push('/photo-save')}
       />
-      <main>
-        <Image
-          src="/vercel.svg"
-          alt="얼굴 사진 결과물"
-          width="43"
-          height="42"
-        />
-        <section>
-          <h2>보정 종류 선택</h2>
-          <ul aria-label="종류 목록">
-            <li aria-label="type item">
-              <button type="button">미백</button>
-              <i>|</i>
-            </li>
-          </ul>
-        </section>
-        <section>
-          <h2>보정하기</h2>
-          {/* eslint jsx-a11y/label-has-associated-control: ["error", { assert: "either" } ] */}
-          <label htmlFor="ratio">0</label>
-          <input type="range" name="ratio" id="ratio" min="0" max="100" />
-          <label htmlFor="ratio">100</label>
-        </section>
+
+      <main className={styles.main}>
+        <div className={styles['face-image-container']}>
+          <div className={styles['face-image']}>
+            <Image src={faceSrc} alt="얼굴 사진 결과물" layout="fill" />
+          </div>
+        </div>
+        <article className={styles['select-container']}>
+          <section>
+            <h2 className={styles['screen-reader-only']}>보정 종류 선택</h2>
+            <ul aria-label="종류 목록" className={styles['type-select']}>
+              {types.map((typeName, idx) => (
+                <RetouchTypes
+                  key={`KEY_${typeName}`}
+                  typeName={typeName}
+                  idx={idx}
+                  types={types}
+                  activeType={activeType}
+                  onClickHandler={onClickHandler}
+                />
+              ))}
+            </ul>
+          </section>
+          <section>
+            <h2 className={styles['screen-reader-only']}>보정하기</h2>
+            {/* eslint jsx-a11y/label-has-associated-control: ["error", { assert: "either" } ] */}
+            {/* <input type="file" id='fileInput' onChange={ handleOnChange}></input> */}
+            <label htmlFor="ratio">0</label>
+            <input
+              className={styles.ratio}
+              type="range"
+              name="ratio"
+              id="ratio"
+              min="0"
+              max="100"
+            />
+            <label htmlFor="ratio">100</label>
+          </section>
+        </article>
       </main>
     </div>
   );
