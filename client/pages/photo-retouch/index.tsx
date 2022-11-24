9dfa9a5fe6cf70b3caf48163b1226aa78bed1f8a
khe <khj778878@naver.com>
2022-11-24 21:27:07 +0900

    Feat: create custom Hook useDebounce

--- client/pages/photo-retouch/index.tsx
+++ client/pages/photo-retouch/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, ChangeEvent } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
@@ -10,20 +10,25 @@ import faceImageState, { withSrc } from 'recoil/faceImage';
 import axios from 'axios';
 import SkinBeautyRange from '@/components/SkinBeautyRange';
 import SlimmingRange from '@/components/SlimmingRange';
+import useDebounce from '../../hooks/useDebounce';
 import styles from './PhotoRetouch.module.css';
 
 const PhotoRetouch: NextPage = () => {
   const faceSrc = useRecoilValue(withSrc);
   const router = useRouter();
-  const [activeType, setActiveType] = useState(0);
+  const [activeType, setActiveType] = useState<number>(0);
   const types: string[] = ['깨끗하게', '슬림하게'];
   const [faceImage, setFaceImage] = useRecoilState(faceImageState);
   const [retouchImageTmp, setRetouchImageTmp] = useState(faceImage);
   const [retouchImageUrlTmp, setRetouchImageUrlTmp] = useState(faceSrc);
-  const [selectType, setSelectType] = useState(0);
-  const [skinValue, setSkinValue] = useState('0');
-  const [slimValue, setSlimValue] = useState('0');
-  const [isCheckBtn, setIsCheckBtn] = useState(false);
+  const [selectType, setSelectType] = useState<number>(0);
+  const [skinValue, setSkinValue] = useState<string>('0');
+  const [slimValue, setSlimValue] = useState<string>('0');
+  const [isCheckBtn, setIsCheckBtn] = useState<boolean>(false);
+  const [rangeValue, setRangeValue] = useState<string[]>(['0', '0']);
+  const debounceSkinValue = useDebounce(rangeValue[0], 800);
+  const debounceSlimValue = useDebounce(rangeValue[1], 800);
+  const [apiCallCount, setApiCallCount] = useState(0);
   // const BEAUTY_API_KEY = process.env.NEXT_PUBLIC_BEAUTY_API_KEY; // 실제 api 호출하려면 주석 풀기
   const FAKE_API_KEY = process.env.NEXT_PUBLIC_FAKE_API_KEY;
 
@@ -33,20 +38,8 @@ const PhotoRetouch: NextPage = () => {
     setRetouchImageUrlTmp(URL.createObjectURL(blob));
   };
 
-  /* 스킨 뷰티 기능 */
-  const handleOnChangeBySkin = async (event: ChangeEvent<HTMLInputElement>) => {
-    const rangeValue: string = event.target.value;
-    if (+rangeValue === 0 && skinValue === '0') return;
-
-    if (+rangeValue === 0 && skinValue !== '0') {
-      setIsCheckBtn(false);
-    }
-    else {
-      setIsCheckBtn(true);
-    }
-    setSkinValue(rangeValue);
-
-    const degree: string = (+rangeValue / 100).toString();
+  const postSkin = () => {
+    const degree: string = (+rangeValue[0] / 100).toString();
     const data = new FormData();
     data.append('image', faceImage as Blob, 'photo.png');
     data.append('retouch_degree', degree);
@@ -56,37 +49,25 @@ const PhotoRetouch: NextPage = () => {
       method: 'POST',
       url: 'https://ai-skin-beauty.p.rapidapi.com/face/editing/retouch-skin',
       headers: {
-        'X-RapidAPI-Key': FAKE_API_KEY, // 실제로 api 호출하려면, BEAUTY_API_KEY로 바꿔넣기
+        'X-RapidAPI-Key': FAKE_API_KEY, // 실제 api 호출하려면 BEAUTY_API_KEY로 바꿔넣기
         'X-RapidAPI-Host': 'ai-skin-beauty.p.rapidapi.com',
       },
       data,
     };
 
     axios
-      .request(options)
-      .then((response) => {
-        const fullImageURL = response.data.data.image_url;
-        const fetchImageURL = fullImageURL.substring(41);
-        fetchImage(fetchImageURL);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    .request(options)
+    .then((response) => {
+      const fullImageURL = response.data.data.image_url;
+      const fetchImageURL = fullImageURL.substring(41);
+      fetchImage(fetchImageURL);
+    })
+    .catch((error) => {
+      global.console.error(error);
+    });
   };
 
-  /* 슬리밍 기능 */
-  const handleOnChangeBySlim = (event: ChangeEvent<HTMLInputElement>) => {
-    const rangeValue: string = event.target.value;
-    if (+rangeValue === 0 && slimValue === '0') return;
-    
-    if (+rangeValue === 0 && slimValue !== '0') {
-      setIsCheckBtn(false);
-    }
-    else {
-      setIsCheckBtn(true);
-    }
-    setSlimValue(rangeValue);
-
+  const postSlim = () => {
     const degree: string = (+rangeValue / 50).toString();
     const data = new FormData();
     data.append('image', faceImage as Blob);
@@ -96,7 +77,7 @@ const PhotoRetouch: NextPage = () => {
       method: 'POST',
       url: 'https://ai-face-slimming.p.rapidapi.com/face/editing/liquify-face',
       headers: {
-        'X-RapidAPI-Key': FAKE_API_KEY,// 실제로 api 호출하려면, BEAUTY_API_KEY로 바꿔넣기
+        'X-RapidAPI-Key': FAKE_API_KEY, // 실제 api 호출하려면 BEAUTY_API_KEY로 바꿔넣기
         'X-RapidAPI-Host': 'ai-face-slimming.p.rapidapi.com',
       },
       data,
@@ -110,20 +91,67 @@ const PhotoRetouch: NextPage = () => {
         fetchImage(fetchImageURL);
       })
       .catch((error) => {
-        console.error(error);
+        global.console.error(error);
       });
   };
 
+  /* 스킨 뷰티 기능 */
+  const handleOnChangeBySkin = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSkinValue(event.target.value);
+    setRangeValue([event.target.value, '0']);
+
+    if (+event.target.value === 0 && skinValue === '0') return;
+
+    if (+event.target.value === 0 && skinValue !== '0') {
+      setIsCheckBtn(false);
+    } else {
+      setIsCheckBtn(true);
+    }
+  };
+
+  useEffect(() => {
+    if (!(apiCallCount === 0 && skinValue === '0')) {
+      setSkinValue(debounceSkinValue);
+      postSkin();
+      setApiCallCount((count) => count + 1);
+    }
+  }, [debounceSkinValue]);
+
+  /* 슬리밍 기능 */
+  const handleOnChangeBySlim = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSlimValue(event.target.value);
+    setRangeValue(['0', event.target.value]);
+
+    if (+event.target.value === 0 && slimValue === '0') return;
+
+    if (+event.target.value === 0 && slimValue !== '0') {
+      setIsCheckBtn(false);
+    } else {
+      setIsCheckBtn(true);
+    }
+  };
+
+  useEffect(() => {
+    if (!(apiCallCount === 0 && slimValue === '0')) {
+      setSlimValue(debounceSlimValue);
+      postSlim();
+      setApiCallCount((count) => count + 1);
+    }
+  }, [debounceSlimValue]);
+
   const onClickHandler = (idx: number) => {
     if (activeType !== idx) {
-      if ((idx === 0 && slimValue === '0') || idx === 1 && skinValue === '0'){
+      if (
+        (idx === 0 && slimValue === '0') ||
+        (idx === 1 && skinValue === '0')
+      ) {
         setActiveType(idx);
         setSelectType(idx);
       }
     }
   };
 
-  const checkHandler = () => {
+  const checkBtnHandler = () => {
     if (selectType === 0) {
       setSkinValue('0');
     } else if (selectType === 1) {
@@ -131,12 +159,13 @@ const PhotoRetouch: NextPage = () => {
     }
     setFaceImage(retouchImageTmp);
     setIsCheckBtn(false);
+    setRangeValue(['0', '0']);
+    setApiCallCount(0);
   };
 
   useEffect(() => {
     setActiveType(0);
     setSelectType(0);
-    setIsCheckBtn(false);
   }, []);
 
   useEffect(() => {
@@ -212,13 +241,13 @@ const PhotoRetouch: NextPage = () => {
                 <button
                   className={styles.activeBtn}
                   type="button"
-                  onClick={checkHandler}
+                  onClick={checkBtnHandler}
                 >
                   적용하기
                 </button>
-              ) :
-                <div className={styles.hiddenBtn}/>
-              }
+              ) : (
+                <div className={styles.hiddenBtn} />
+              )}
             </div>
           </section>
         </article>
